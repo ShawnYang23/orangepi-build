2e9538b65d36f0b1c89e8ae71282ac8e0b3ff06f
Edward Lai <edward@vibe.us>
2023-04-24 16:52:01 +0800

    custom image build scripts

--- scripts/debootstrap.sh
+++ scripts/debootstrap.sh
@@ -68,9 +68,11 @@ PRE_INSTALL_DISTRIBUTION_SPECIFIC
 
 		#[[ $EXTERNAL_NEW == prebuilt ]] && chroot_installpackages "yes"
 
-		# stage: user customization script
-		# NOTE: installing too many packages may fill tmpfs mount
-		customize_image
+	echo "VIBE ----> customize img!!"
+	# stage: user customization script
+	# NOTE: installing too many packages may fill tmpfs mount
+	customize_image
+	echo "VIBE ----> done img!!"
 
 		# remove packages that are no longer needed. Since we have intrudoced uninstall feature, we might want to clean things that are no longer needed
 		display_alert "No longer needed packages" "purge" "info"
